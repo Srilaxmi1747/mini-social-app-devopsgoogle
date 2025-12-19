645dc2163b88e892013ded903d69fac7e03fafb8
Srilaxmi1747 <srilaxmi.veeramalla@gmail.com>
2025-12-19 15:43:17 +0530

    Update Post.cs

--- src/MiniSocialApp/Models/Post.cs
+++ src/MiniSocialApp/Models/Post.cs
@@ -3,7 +3,8 @@ namespace MiniSocialApp.Models
 {
     public class Post
     {
-        public string Title { get; set; }
-        public string Content { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
     }
 }
