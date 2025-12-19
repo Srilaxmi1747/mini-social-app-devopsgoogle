5f8fbfa7e47dd85da46b2100953a0a804c4568f7
Srilaxmi1747 <srilaxmi.veeramalla@gmail.com>
2025-12-19 13:26:23 +0530

    Update PostController.cs

--- src/MiniSocialApp/Controllers/PostController.cs
+++ src/MiniSocialApp/Controllers/PostController.cs
@@ -1,10 +1,30 @@
 using Microsoft.AspNetCore.Mvc;
+using MiniSocialApp.Models;
 
 namespace MiniSocialApp.Controllers
 {
     public class PostController : Controller
     {
-        public IActionResult Feed() => View();
+        private static List<Post> posts = new();
+
+        public IActionResult Index()
+        {
+            return View(posts);
+        }
+
+        [HttpGet]
         public IActionResult Create() => View();
+
+        [HttpPost]
+        public IActionResult Create(string content)
+        {
+            posts.Add(new Post
+            {
+                Content = content,
+                CreatedAt = DateTime.Now
+            });
+
+            return RedirectToAction("Index");
+        }
     }
 }
