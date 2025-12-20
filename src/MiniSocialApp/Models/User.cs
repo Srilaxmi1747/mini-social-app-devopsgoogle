a26a13ab223c136f0c005a78a85409ce817f19ba
Srilaxmi1747 <srilaxmi.veeramalla@gmail.com>
2025-12-20 17:01:33 +0530

    Update User.cs

--- src/MiniSocialApp/Models/User.cs
+++ src/MiniSocialApp/Models/User.cs
@@ -2,7 +2,10 @@ namespace MiniSocialApp.Models
 {
     public class User
     {
-        public string Email { get; set; }
-        public string Password { get; set; }
+        public string FirstName { get; set; } = "";
+        public string LastName { get; set; } = "";
+        public string Email { get; set; } = "";
+        public string PhoneNumber { get; set; } = "";
+        public string Password { get; set; } = "";
     }
 }
